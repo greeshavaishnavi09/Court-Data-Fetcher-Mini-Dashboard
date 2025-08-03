5cc38f52ebe615786591e61109c4c69b9af2bc49
Greesha Vaishnavi <greeshavaishnavi09@gmail.com>
2025-08-03 22:54:00 +0530

    Add Dockerfile and unit test for Court Data Fetcher

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+class CourtDashboardTest(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard_page(self):
+        response = self.client.get('/dashboard')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
